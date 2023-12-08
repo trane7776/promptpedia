eb70123946067948991f3392de5f439c5f0bdf5d
trane7776 <izbasarov.t@edu.rea.ru>
2023-12-09 00:50:13 +0300

    added some styles to nav bar

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+import Feed from "@/components/Feed";
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Уникальное пространство. Находи и делись
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">ИИ промптами</span>
+      </h1>
+      <p className="desc text-center">
+        Добро пожаловать на Промптопедию – уникальное пространство для обмена
+        идеями, вдохновением и творческими промптами.
+      </p>
+      <Feed />
+    </section>
+  );
+};
+
+export default Home;
---
+++ components/Nav.jsx
@@ -0,0 +1,84 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+const Nav = () => {
+  const isUserLoggedIn = true;
+  const [providers, setProviders] = useState(null);
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+      setProviders(response);
+    };
+    setProviders();
+  }, []);
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="logo"
+          width={30}
+          height={30}
+        />
+        <p className="logo_text">Промтопедия</p>
+      </Link>
+      {/* Desktop Navigation */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Создать пост
+            </Link>
+            <button type="button" onClick={signOut} className="outline_btn">
+              Выйти
+            </button>
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                alt="profile"
+                width={37}
+                height={37}
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => {
+                return (
+                  <button
+                    type="button"
+                    key="provider.name"
+                    className="black_btn"
+                    onClick={() => signIn(provider.id)}
+                  >
+                    Войти
+                  </button>
+                );
+              })}
+          </>
+        )}
+      </div>
+      {/* Mobile Navigation */}
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <div className="flex">
+            <Image
+              src="/assets/images/logo.svg"
+              alt="profile"
+              width={37}
+              height={37}
+              className="rounded-full"
+            />
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
